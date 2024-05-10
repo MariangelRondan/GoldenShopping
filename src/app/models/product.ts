f424a9f1193f8d79e2b8ac1d169b2717089a9bcb
MariangelRondan <rondanmariangel@gmail.com>
2024-05-10 15:31:39 -0300

    Golder Shopping

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    id: number = 0;
+    title:string = '';
+    price: number = 0;
+    category: {name: string} ={name: ''};
+    images: [string] =[''];
+
+}
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'https://api.escuelajs.co/api/v1'
+
+};
